0f378c18a7b4b41c1ecf7970f6d630a042b71dce
Arun <your-email@example.com>
2026-06-10 13:16:24 +0530

    fix: add CORS headers

--- next.config.ts
+++ next.config.ts
@@ -10,9 +10,10 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://pro-health-track-arun-kumar1920.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
         ],
       },
     ];
